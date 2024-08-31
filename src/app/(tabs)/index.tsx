d5a72c9c2a378bf0bad3ca4a7a8b1389a33db434
pieceofGithub <demiroglu.webworld@gmail.com>
2024-08-31 19:48:08 +0300

    hello github

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,14 +1,12 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import { StyleSheet } from "react-native";
+import { Text, View } from "@/src/components/Themed";
+import Colors from "@/src/constants/Colors";
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Text style={styles.title}>Hello github</Text>
+      <Text style={styles.price}>price: 19.99$</Text>
     </View>
   );
 }
@@ -16,16 +14,20 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
+    marginVertical: 10,
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
+  },
+  price: {
+    color: Colors.light.tint,
   },
 });
